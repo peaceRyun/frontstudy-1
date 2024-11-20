5378ad25f25894d0abf4fcb81a51f2c1645d4752
Yoongu Kwon <dbsrn1110@gmail.com>
2024-11-20 11:48:24 +0900

    18-1

--- 04-js/js/09-비동기.js
+++ 04-js/js/09-비동기.js
@@ -67,8 +67,16 @@
 const getUser = (userId) => {
     return new Promise((resolve, reject) => {
         fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`)
-            .then((res) => res.json())
-            .then((data) => resolve(data));
+            .then((res) => {
+                // 실패시 처리
+                if (!res.ok) {
+                    throw new Error(`${userId}번 유저를 찾을 수 없습니다.`);
+                }
+                // 성공시 처리
+                return res.json();
+            })
+            .then((data) => resolve(data))
+            .catch((error) => reject(error));
     });
 };
 
@@ -83,4 +91,60 @@ getUser(1)
     })
     .then((data) => {
         console.log(data.title);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    // 무조건 실행
+    .finally(() => {
+        console.log('모두 완료');
     });
+
+// async await로 수정해보자
+const printUser = async () => {
+    try {
+        const user1 = await getUser(1);
+        console.log(user1.title);
+
+        const user2 = await getUser(2);
+        console.log(user2.title);
+
+        const user3 = await getUser(3);
+        console.log(user3.title);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        console.log('모두 완료');
+    }
+};
+
+printUser();
+
+// aa, bb
+
+const aa = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('aa');
+            resolve();
+        }, 1000);
+    });
+};
+const bb = () => {
+    console.log('bb');
+};
+
+// aa().then(() => {
+//     return bb();
+// });
+
+const main = async () => {
+    await aa();
+    bb();
+};
+
+// fetch 함수를 사용하여 JSONPlaceholder API를 호출합니다.
+// console.log(fetch('https://jsonplaceholder.typicode.com/users'));
+
+// 결과값
+fetch('https://jsonplaceholder.typicode.com/users').then((res) => console.log(res.json()));
