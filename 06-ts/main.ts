9bad39cac01d6ef2ba2d3c5eb403f7a663ec43ea
Yoongu Kwon <dbsrn1110@gmail.com>
2024-12-10 15:45:23 +0900

    19-2

--- 06-ts/main.ts
+++ 06-ts/main.ts
@@ -48,3 +48,64 @@ interface Person {
 
 const user1: Person = { name: '김겨울', age: 30 };
 const user2: Person = { name: '김여름', age: 20 };
+
+//객체 타입
+const a: { name: string; age: number } = { name: 'kim', age: 20 };
+const b: { name: string; age: number } = { name: 'jeong', age: 30 };
+
+// interface
+interface Ab {
+    name: string;
+    age: number;
+}
+
+const aa: Ab = { name: 'kim', age: 20 };
+const bb: Ab = { name: 'jeong', age: 30 };
+
+// 배열 타입
+const arr1: number[] = [1, 2, 3];
+const arr2: string[] = ['가', '나', '다'];
+const arr3: any[] = [1, '가', true];
+
+// 객체 배열 타입
+interface UserProp {
+    name: string;
+    age: number;
+}
+const user: UserProp[] = [
+    { name: '김가을', age: 3 },
+    { name: '김겨을', age: 2 },
+];
+
+// 객체 내부 객체 타입
+interface Address {
+    city: string;
+    dong: string;
+}
+
+interface User3 {
+    name: string;
+    age?: number;
+    address: Address;
+}
+
+const user3: User3 = {
+    name: '김봄',
+    address: {
+        city: 'seoul',
+        dong: 'gangnam',
+    },
+};
+
+//일반 함수
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(sum(1, 2));
+
+//화살표 함수 타입
+const sum2: (a: number, b: number) => number = (a, b) => {
+    return a + b;
+};
+console.log(sum2(1, 2));
